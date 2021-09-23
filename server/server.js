59423a8675f8f456190b63ef8ada7638df9c82b2
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-09-23 14:06:01 +1200

    appbar added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path')
 
-const routes = require('./routes/routes')
+// const routes = require('./routes/routes')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/routes', routes)
+// server.use('/api/v1/routes', routes)
 
 module.exports = server
