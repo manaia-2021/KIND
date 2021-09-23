a930d8cfe8266995bf9e448ec04f2d8dacb7f08d
Brad Booysen <45187227+brad-booysen@users.noreply.github.com>
2021-09-23 16:11:33 +1200

    Database functions

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,97 @@
-// const connection = require('./connection')
 
-// function (db = connection) {
-//   return db().select()
-// }
+const knex = require('knex')
+const config = require('./knexfile').development
+const database = knex(config)
 
-// module.exports = {
-// }
+module.exports = {
+    getAllActions,
+    getAllCategories,
+    getAllUsers,
+    getUser,
+    deleteUser,
+    getUsersByPoints,
+    updateUserAction,
+    addNewUserAction,
+    addNewUser,
+    getActionsByCategory,
+    getUserActionByUser
+}
+
+
+// Add a new user
+function addNewUser(user, db = connection) {
+    return db('users')
+    .insert(user)
+}
+
+// Get all users
+function getAllUsers(db  = database) {
+    return db('users')
+    .select()
+}
+
+// Get a specific user
+function getUser(id, db  = database) {
+    return db('users')
+    .first()
+    .where('id', id)
+}
+
+// Get users ordered by highest number of points for the leaderboard
+function getUsersByPoints(db = connection) {
+    return db('users')
+    .select()
+    .orderBy('points', 'desc')
+}
+
+// Delete user
+function deleteUser (id, db = database) {
+    return db('users')
+      .where('id', id)
+      .del()
+}
+
+// Get all categories
+function getAllCategories(db  = database) {
+    return db('category')
+    .select()
+}
+
+// Get all actions
+function getAllActions(db  = database) {
+    return db('action')
+    .select()
+}
+
+// Get actions by category
+function getActionsByCategory(id, db  = database) {
+    return db('action')
+    .select()
+    .where('category_id', id)
+}
+
+// Get user action by user id
+function getUserActionByUser(id, db  = database) {
+    return db('user_action')
+    .select()
+    .where('user_id', id)
+}
+
+// Add new action to user
+function addNewUserAction(userId, actionId, db  = database) {
+    return db('user_action')
+    .insert({user_id: userId, action_id: actionId})
+}
+
+// Update user action
+function updateUserAction(id, status, db  = database) {
+    return db('user_action')
+    .first()
+    .where('id', id)
+    .update( {
+        completed: status 
+    })
+}
+
+
+
