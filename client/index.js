fb75c489d287cf0c8d90bff89778927ffae96a45
kerry-davis <pulsta@gmail.com>
2021-09-23 16:06:43 +1200

    remove unwanted npm libraries

--- client/index.js
+++ client/index.js
@@ -3,17 +3,11 @@ import { render } from 'react-dom'
 // import { Provider } from 'react-redux'
 import { Auth0Provider } from '@auth0/auth0-react';
 // import store from './store'
+
+//required for auth0
 import config from "../auth_config.json";
 
 import App from './components/App'
-
-//.env variables for Auth
-// const REACT_APP_AUTH0_DOMAIN=dev-zl36toh0.us.auth0.com
-// const REACT_APP_AUTH0_CLIENT_ID=scHKSdELqiR7QFKD4ftPqQ07L3wsRtda
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
-// const domain = 'dev-zl36toh0.us.auth0.com'
-// const clientId = 'scHKSdELqiR7QFKD4ftPqQ07L3wsRtda'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
