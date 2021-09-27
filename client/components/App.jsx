0529df76cb5436a7d15bc3d0f76ac7cb60e4b050
Brad Booysen <45187227+brad-booysen@users.noreply.github.com>
2021-09-27 13:28:56 +1300

    Redirect user based on login status

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, useHistory } from 'react-router-dom'
 
-import { useAuth0, loginWithRedirect } from '@auth0/auth0-react'
+import { useAuth0 } from '@auth0/auth0-react'
 
 import Loading from '../components/Loading'
 import HeaderBar from '../components/HeaderBar'
@@ -21,12 +21,12 @@ function App () {
       if (isAuthenticated) {
         history.push('/profile')
       } else {
-        loginWithRedirect()
+        history.push('/')
       }
     }
 
     checkUser()
-  }, [isAuthenticated, loginWithRedirect])
+  }, [isAuthenticated])
 
   if (isLoading) {
     return <Loading />
