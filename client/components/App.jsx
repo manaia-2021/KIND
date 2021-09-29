99f2d9c6b1371b8ad7cac7f2307a9499d290a4d6
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-09-29 15:46:52 +1300

    test

---
+++ client/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Route } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+
+import ProtectedRoute from '../auth/ProtectedRoute'
+import HeaderBar from '../components/HeaderBar'
+import LandingPage from './LandingPage'
+import CategoriesPage from './CategoriesPage'
+// import UserDashboard from './UserDashboard'
+import Leaderboard from './LeaderBoard'
+
+import { fetchUserProfile } from '../actions/user'
+import ProfilePage from './ProfilePage'
+
+function App (props) {
+  const { user, isAuthenticated } = useAuth0()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const { name, email } = user
+      props.dispatch(fetchUserProfile({ name, email }))
+    }
+  }, [isAuthenticated])
+
+  return (
+    <>
+      <Route path='/' component={HeaderBar} />
+      <Route exact path='/' component={LandingPage} />
+      <Route path='/leaderboard' component={Leaderboard} />
+      <ProtectedRoute path='/categories' component={CategoriesPage} />
+      <ProtectedRoute path='/profile' component={ProfilePage} />
+    </>
+  )
+}
+
+export default connect()(App)
