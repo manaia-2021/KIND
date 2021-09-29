b1bd9983966c8dcb99cfa00e97acb2ef6becc5d1
kerry-davis <pulsta@gmail.com>
2021-09-29 13:53:45 +1300

    testing HeaderBar done

---
+++ client/components/HeaderBar.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { screen, render, fireEvent } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import store from '../store'
+
+import HeaderBar from './HeaderBar'
+
+const mockHistoryPush = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush
+  })
+}))
+
+test('HeaderBar component has loaded', () => {
+  render(<Provider store={store}><HeaderBar /></Provider>)
+
+  const heading = screen.getByText(/KIND/)
+  expect(heading).not.toBeUndefined()
+})
+
+test('HeaderBar click KIND button', () => {
+  render(<Provider store={store}><HeaderBar /></Provider>)
+
+  const button = screen.getByText('KIND')
+  fireEvent.click(button)
+  expect(mockHistoryPush).toHaveBeenCalledWith('/')
+})
+
+test('HeaderBar click Leaderboard button', () => {
+  render(<Provider store={store}><HeaderBar /></Provider>)
+
+  const button = screen.getByText('Leaderboard')
+  fireEvent.click(button)
+  expect(mockHistoryPush).toHaveBeenCalledWith('/leaderboard')
+})
